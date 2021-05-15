167be1c72feb907c1e08e08eef0734faf77708bf
Nikita Rychagov <zerganrus@gmail.com>
2021-05-15 22:40:15 +0300

    init

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+import os 
+from _thread import *
+
+# Socket Programming-> It helps us to connect a client to a server. 
+# Client is message sender and receiver and server is just a listener that works on data sent by client.
+
+# What is a Thread?
+# A thread is a light-weight process that does not require much memory overhead,
+# they are cheaper than processes.
+
+# What is Multi-threading Socket Programming?
+# Multithreading is a process of executing multiple threads simultaneously in a single process.
+
+# Multi-threading Modules :
+# A _thread module & threading module is used for multi-threading in python, 
+# these modules help in synchronization and provide a lock to a thread in use.
+
